e1ad9c7f824948d3ae894abda7b4170762084504
Joeun <joyfuljoeunlee@gmail.com>
2022-01-14 16:05:34 +0900

    Tailwind CSS 세팅

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
 // custom typefaces
-import "typeface-montserrat"
 import "typeface-merriweather"
+import "typeface-montserrat"
+import "./src/global.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
