6b9c6b264b38dc9939f7337ae9e5b86177facb4d
Joeun <joyfuljoeunlee@gmail.com>
2022-02-17 22:46:10 +0900

    / 진입하면 /about 으로 replace

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import { graphql } from "gatsby"
-import * as React from "react"
+import { graphql, navigate } from "gatsby"
+import React, { useEffect } from "react"
 import About from "./about"
 
 const Index = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
+
+  useEffect(() => {
+    navigate("/about", { replace: true })
+  }, [])
 
   return <About data={data} location={location} title={siteTitle} />
 }
