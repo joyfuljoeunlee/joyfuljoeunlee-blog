42ea332f81f53efb6c9b612900a8f1ac573bc304
Joeun <joyfuljoeunlee@gmail.com>
2022-03-16 17:59:42 +0900

    StaticQuery 적용

--- src/components/templates/BlogPostTemplate.js
+++ src/components/templates/BlogPostTemplate.js
@@ -1,47 +1,47 @@
-import { graphql } from "gatsby"
-import * as React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import React from "react"
 import useSiteMetadata from "../../hooks/useSiteMetadata"
 import Layout from "../Layout"
 import ScrollProgressBar from "../ScrollProgressBar"
 
-const BlogPostTemplate = ({ data, location }) => {
-  const post = data.ghostPost
-
+const BlogPostTemplate = ({ location }) => {
   const { defaultTitle } = useSiteMetadata()
 
   return (
-    <Layout
-      location={location}
-      title={defaultTitle}
-      seoTitle={post.title}
-      seoDescription={post.excerpt}
-    >
-      <ScrollProgressBar />
-      <article>
-        <header className="grid gap-9 pt-12 pb-24 text-center">
-          <h1 className="text-6xl font-bold">{post.title}</h1>
-          <p>{post.published_at_pretty}</p>
-        </header>
-        <section
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-      </article>
-    </Layout>
+    <StaticQuery
+      query={graphql`
+        query {
+          ghostPost {
+            title
+            slug
+            excerpt
+            published_at_pretty: published_at(formatString: "DD MMMM, YYYY")
+            html
+          }
+        }
+      `}
+      render={data => (
+        <Layout
+          location={location}
+          title={defaultTitle}
+          seoTitle={data.ghostPost.title}
+          seoDescription={data.ghostPost.excerpt}
+        >
+          <ScrollProgressBar />
+          <article>
+            <header className="grid gap-9 pt-12 pb-24 text-center">
+              <h1 className="text-6xl font-bold">{data.ghostPost.title}</h1>
+              <p>{data.ghostPost.published_at_pretty}</p>
+            </header>
+            <section
+              className="post-content"
+              dangerouslySetInnerHTML={{ __html: data.ghostPost.html }}
+            />
+          </article>
+        </Layout>
+      )}
+    />
   )
 }
 
 export default BlogPostTemplate
-
-export const pageQuery = graphql`
-  query ($slug: String!) {
-    ghostPost(slug: { eq: $slug }) {
-      id
-      title
-      slug
-      excerpt
-      published_at_pretty: published_at(formatString: "DD MMMM, YYYY")
-      html
-    }
-  }
-`
