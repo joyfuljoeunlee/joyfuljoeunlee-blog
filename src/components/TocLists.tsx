1f10a816bef8469bbda1af507f3d37dee664b534
Joeun <joyfuljoeunlee@gmail.com>
2022-04-13 19:40:26 +0900

    style: toc style 변경

--- src/components/TocLists.tsx
+++ src/components/TocLists.tsx
@@ -10,15 +10,17 @@ const TocLists = ({ tocLists }: Props) => {
       <p className="mt-16 mb-6 text-2xl font-bold laptop:mt-24 tablet:mt-20 laptop:mb-8 tablet:mb-7 laptop:text-3xl">
         목차
       </p>
-      {tocLists?.map((list, index) => {
-        const tocList = list.innerHTML
-        const id = list.id
-        return (
-          <li key={index} className="text-lg underline">
-            <a href={`#${id}`}>{tocList}</a>
-          </li>
-        )
-      })}
+      <ol className="list-decimal list-inside">
+        {tocLists?.map((list, index) => {
+          const tocList = list.innerHTML
+          const id = list.id
+          return (
+            <li key={index} className="text-lg underline underline-offset-2">
+              <a href={`#${id}`}>{tocList}</a>
+            </li>
+          )
+        })}
+      </ol>
     </>
   )
 }
