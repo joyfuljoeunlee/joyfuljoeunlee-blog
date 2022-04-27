3ebd5fdf16f875ff01e5477b28f424aca78f4d9b
Joeun <joyfuljoeunlee@gmail.com>
2022-04-27 15:22:33 +0900

    feat: toc 링크에만 scroll-smooth 적용

--- src/components/TocLists.tsx
+++ src/components/TocLists.tsx
@@ -5,6 +5,11 @@ interface Props {
 }
 
 const TocLists = ({ tocLists }: Props) => {
+  const goToContents = (id: string) => {
+    const destination = document.getElementById(`${id}`)
+    destination?.scrollIntoView({ behavior: "smooth" })
+  }
+
   return (
     <>
       <p className="mt-16 mb-6 text-2xl font-bold laptop:mt-24 tablet:mt-20 laptop:mb-8 tablet:mb-7 laptop:text-3xl">
@@ -15,8 +20,14 @@ const TocLists = ({ tocLists }: Props) => {
           const tocList = list.innerHTML
           const id = list.id
           return (
-            <li key={index} className="text-lg underline underline-offset-2">
-              <a href={`#${id}`}>{tocList}</a>
+            <li
+              key={index}
+              className="text-lg underline underline-offset-2"
+              onClick={() => {
+                goToContents(id)
+              }}
+            >
+              <span className="cursor-pointer">{tocList}</span>
             </li>
           )
         })}
